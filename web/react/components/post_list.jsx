32359b7298dfd8ff25ceb841cb16ebe289a97c1a
Antti Ahti <antti.ahti@gmail.com>
2015-11-09 14:52:50 +0200

    Try to fix top cutting out sometimes
    Sometimes when there are new messages the layout breaks a little. I tracked this down to scrollIntoView().
    
    https://trello.com/c/0yiZDAV5/50-bugi8-layoutti-menee-vahan-yli-ylareunasta-valilla

--- web/react/components/post_list.jsx
+++ web/react/components/post_list.jsx
@@ -237,10 +237,42 @@ export default class PostList extends React.Component {
         postHolder[0].scrollTop = val;
     }
     scrollToBottom(force) {
+
+        // Quick hack.
+        // Source: https://gist.github.com/hsablonniere/2581101
+        if (!Element.prototype.scrollIntoViewIfNeeded) {
+          Element.prototype.scrollIntoViewIfNeeded = function (centerIfNeeded) {
+            centerIfNeeded = arguments.length === 0 ? true : !!centerIfNeeded;
+
+            var parent = this.parentNode,
+                parentComputedStyle = window.getComputedStyle(parent, null),
+                parentBorderTopWidth = parseInt(parentComputedStyle.getPropertyValue('border-top-width')),
+                parentBorderLeftWidth = parseInt(parentComputedStyle.getPropertyValue('border-left-width')),
+                overTop = this.offsetTop - parent.offsetTop < parent.scrollTop,
+                overBottom = (this.offsetTop - parent.offsetTop + this.clientHeight - parentBorderTopWidth) > (parent.scrollTop + parent.clientHeight),
+                overLeft = this.offsetLeft - parent.offsetLeft < parent.scrollLeft,
+                overRight = (this.offsetLeft - parent.offsetLeft + this.clientWidth - parentBorderLeftWidth) > (parent.scrollLeft + parent.clientWidth),
+                alignWithTop = overTop && !overBottom;
+
+            if ((overTop || overBottom) && centerIfNeeded) {
+              parent.scrollTop = this.offsetTop - parent.offsetTop - parent.clientHeight / 2 - parentBorderTopWidth + this.clientHeight / 2;
+            }
+
+            if ((overLeft || overRight) && centerIfNeeded) {
+              parent.scrollLeft = this.offsetLeft - parent.offsetLeft - parent.clientWidth / 2 - parentBorderLeftWidth + this.clientWidth / 2;
+            }
+
+            if ((overTop || overBottom || overLeft || overRight) && !centerIfNeeded) {
+              this.scrollIntoView(alignWithTop);
+            }
+          };
+        }
+
         this.isUserScroll = false;
         var postHolder = $(React.findDOMNode(this.refs.postlist));
         if ($('#new_message_' + this.props.channelId)[0] && !this.userHasSeenNew && !force) {
-            $('#new_message_' + this.props.channelId)[0].scrollIntoView();
+            //$('#new_message_' + this.props.channelId)[0].scrollIntoView();
+            $('#new_message_' + this.props.channelId)[0].scrollIntoViewIfNeeded();
         } else {
             postHolder.addClass('hide-scroll');
             postHolder[0].scrollTop = postHolder[0].scrollHeight;
